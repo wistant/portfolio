ffbdd91a9c206a4d5761021fbdd753e8c0670243
Wistant Kode <wistantkode@protonmail.com>
2026-06-03 02:13:35 +0100

    feat(sponsors): add sponsors preview panel to home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -10,6 +10,7 @@ import { Insights } from "./components/insights"
 import { ProfileHeader } from "./components/profile-header"
 import { Projects } from "./components/projects"
 import { SocialLinks } from "./components/social-links"
+import { Sponsors } from "./components/sponsors"
 import { TechStack } from "./components/tech-stack"
 
 export const metadata: Metadata = {
@@ -56,7 +57,7 @@ export default function HomePage() {
         <Experiences />
         <Separator />
 
-        {/*<Sponsors />*/}
+        <Sponsors />
         <Separator />
 
         <Separator />
