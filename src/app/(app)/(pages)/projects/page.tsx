a38f5379ef34254e21bf1b1caec35634a09bdcc8
Wistant Kode <wistantkode@protonmail.com>
2026-06-13 03:51:21 +0100

    style(app): format page.tsx

--- src/app/(app)/(pages)/projects/page.tsx
+++ src/app/(app)/(pages)/projects/page.tsx
@@ -102,7 +102,7 @@ export default function Page() {
               Open Source Engines & Tools 💻
             </h2>
           </div>
-          <div className="relative py-4 border-b border-line">
+          <div className="relative border-b border-line py-4">
             {/* Background Vertical Separator Lines */}
             <div className="pointer-events-none absolute inset-0 -z-1 grid grid-cols-1 gap-4 max-sm:hidden sm:grid-cols-2">
               <div className="border-r border-line"></div>
@@ -140,7 +140,7 @@ export default function Page() {
               Client & Company Architectures 🏢
             </h2>
           </div>
-          <div className="relative py-4 border-b border-line">
+          <div className="relative border-b border-line py-4">
             {/* Background Vertical Separator Lines */}
             <div className="pointer-events-none absolute inset-0 -z-1 grid grid-cols-1 gap-4 max-sm:hidden sm:grid-cols-2">
               <div className="border-r border-line"></div>
@@ -178,7 +178,7 @@ export default function Page() {
               Landings, Frontend & Experiential Apps 🎨
             </h2>
           </div>
-          <div className="relative py-4 border-b border-line">
+          <div className="relative border-b border-line py-4">
             {/* Background Vertical Separator Lines */}
             <div className="pointer-events-none absolute inset-0 -z-1 grid grid-cols-1 gap-4 max-sm:hidden sm:grid-cols-2">
               <div className="border-r border-line"></div>
